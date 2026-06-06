f4434b8ffed99dd6dedaf2363f5c98e7e7bd3883
Khyati Bhaardwaj <bhaardwajkhyati@gmail.com>
2026-06-06 17:13:01 +0000

    Adding the fog animation

--- gc/src/app/page.js
+++ gc/src/app/page.js
@@ -1,28 +1,62 @@
 'use client'
+import Fog from './fog'
 import {motion} from 'framer-motion'
+
+const title = "Grief Cartographer"
+
+const letterVariants = {
+  hidden: {opacity: 0},
+  visible: {opacity: 1},
+}
+
+const containerVariants = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.08,
+    },
+  },
+}
 
 export default function Home(){
   return (
     <main className = "min-h-screen bg-black flex flex-col items-center justify-center ">
-      {/* Title */}
+      {/*fog*/}
+      <div className ="absolute inset-0 z-0"> <Fog/> </div>
+      
+      <div className="relative z-10 flex flex-col items-center">
+
+        {/* Title */}
       <motion.h1
-      initial = {{ opacity: 0}}
-      animate = {{ opacity:1 }}
-      transition= {{duration: 2.5}}
-      className = "text-white text-4xl font-light tracking-widest mb-6"
+      variants = {containerVariants}
+      initial = "hidden"
+      animate = "visible"
+      className = "text-white text-4xl font-light tracking-widest mb-6 flex"
       >
-        Grief Cartographer
+        {title.split("").map((char,index)=>(
+          <motion.span
+          key={index}
+          variants = {letterVariants}
+          transition={{ duration:0.3}}>
+            {char === " " ? "\u00A0" : char}
+          </motion.span>
+        ))}
       </motion.h1>
       {/* Still Here */}
       <motion.p
-        initials={{ opacity: 0}}
-        animate={{opacity:1}}
-        transition={{ duration: 2.5, delay: 3}}
-        className=" text-grey-600 text-4xl font-light tracking-widest font-[family-name:var(--font-dancing)]"
+        initial={{ opacity: 0, filter: "blur(12px)"}}
+        animate={{opacity:1 , filter:"blur(0px)"}
+      }
+        transition={{ duration: 3, delay: 0.5}}
+        className=" text-Gray-600 text-4xl font-light tracking-widest font-[family-name:var(--font-dancing)]"
       >
         Still Here.
       </motion.p>
 
+
+
+      </div>
+      
     </main>
   )
 }
